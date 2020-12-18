1f1e1962d11f8484e6ff962967e56bafd950e7c3
Suparna <suparnaarya1997@gmail.com>
2020-12-18 16:31:12 +0530

    Intial Commit

---
+++ src/main/java/com/cg/timecardapi/TimecardapiApplication.java
@@ -0,0 +1,13 @@
+package com.cg.timecardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecardapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecardapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/timecardapi/TimecardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.timecardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
